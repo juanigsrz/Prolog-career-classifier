853dd186eeb930c1617c272b6177d55c447a6063
Juan Ignacio Suarez <juanigsrz@gmail.com>
2019-04-25 21:58:08 -0300

    Finished the database structure

--- src/database.pl
+++ src/database.pl
@@ -4,12 +4,12 @@ investigacion :- verify('ambito_investigacional').
 otro          :- verify('otro_ambito').
 
 /* Perfil personal */
-organizador :- industria,     verify('manejar_informacion').
-hacedor     :- industria,     verify('construir_soluciones'). 
-persuasor   :- industria,     verify('relaciones_sociales').
-pensador    :- investigacion, verify('manejar_informacion').
-ayudante    :- otro,          verify('relaciones_sociales').
-creador     :- otro,          verify('construir_soluciones').
+organizador :- industria,                     verify('manejar_informacion').
+hacedor     :- (industria, !; investigacion), verify('construir_soluciones'). 
+persuasor   :- industria,                     verify('relaciones_sociales').
+pensador    :- investigacion,                 verify('manejar_informacion').
+ayudante    :- otro,                          verify('relaciones_sociales').
+creador     :- otro,                          verify('construir_soluciones').
 
 /* Orientacion */
 exactas        :- (pensador, !; hacedor),      verify('ciencia').
